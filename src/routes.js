2d2f1803f373086de3411307449f89f224069a68
defauth98 <neto.daniribeiro@gmail.com>
2021-09-01 16:31:33 -0300

    Requisito 3: login

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,14 @@ const { Router } = require('express');
 
 const getAllTalkers = require('./getAllTalkers');
 const getTalkerById = require('./getTalkerById');
+const login = require('./login');
 
 const routes = Router();
 
 routes.get('/talker', getAllTalkers);
 
 routes.get('/talker/:id', getTalkerById);
+
+routes.post('/login', login);
 
 module.exports = routes;
