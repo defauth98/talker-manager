07617f110db8bb78d0966cec3c3ae43803bc7e0a
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-02-21 10:50:09 -0300

    talkerService -  search talker

---
+++ tests/unit/talkerService/searchTalker.test.js
@@ -0,0 +1,32 @@
+const TalkerService = require("../../../src/services/talker")
+const { validUserInfo } = require("../../utils/userData")
+
+describe('SearchTalker', () => { 
+  it("should not find a talker with no token", () => {
+    const searchTalkerResponse = TalkerService.searchTalker('')
+
+    expect(searchTalkerResponse.status).toEqual(401)
+    expect(searchTalkerResponse.message).toEqual('Token não encontrado')
+  })
+
+  it("should not find a talker with invalid token", () => {
+    const searchTalkerResponse = TalkerService.searchTalker('invalid_token')
+
+    expect(searchTalkerResponse.status).toEqual(401)
+    expect(searchTalkerResponse.message).toEqual('Token inválido')
+  })
+
+  it("should not find a talker with invalid name", () => {
+    const searchTalkerResponse = TalkerService.searchTalker(validUserInfo, "invalid_search")
+
+    expect(searchTalkerResponse.status).toEqual(200)
+    expect(searchTalkerResponse.result.length).toEqual(0)
+  })
+
+  it("should  find a talker with valid name", () => {
+    const searchTalkerResponse = TalkerService.searchTalker(validUserInfo, "Heloísa")
+
+    expect(searchTalkerResponse.status).toEqual(200)
+    expect(searchTalkerResponse.result.length).toBeGreaterThanOrEqual(1)
+  })
+})
